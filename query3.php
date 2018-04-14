7d64cf0774eda34556d53a2cffc460931feb2027
WYHNUS <yanhao0321@gmail.com>
2018-04-14 23:21:07 +0800

    implement query 3

--- query3.php
+++ query3.php
@@ -10,6 +10,22 @@
   include('filter.html');
 ?>
 
+<table id="query_3_table" class="table" style="margin: 20px; display: none">
+  <thead>
+    <th>Total Extended Prices</th>
+    <th>Discounted Extended Prices</th>
+    <th>Discounted Extended Prices plus Tax</th>
+    <th>Average quantity</th>
+    <th>Average Extended Prices</th>
+    <th>Average Discount</th>
+    <th>Year</th>
+    <th>Month</th>
+    <th>Lineitems Count</th>
+  </thead>
+  <tbody id="query_3_table_content">
+  </tbody>
+</table>
+
 <script>
   $(() => {
     $('#filter_form').on('submit', (e) => {
@@ -26,7 +42,24 @@
         url: 'server.php',
         data: serializedData,
         success: (result) => {
-          console.log(result)
+          let content = JSON.parse(result)
+          $('#query_3_table').show()
+          
+          let tableBody = $('#query_3_table_content')
+          tableBody.empty()
+          for (let row of content) {
+            var tableRow = $('<tr></tr>')
+            tableRow.append('<td>' + row.price_sum + '</td>')
+            tableRow.append('<td>' + row.discounted_price_um + '</td>')
+            tableRow.append('<td>' + row.discounted_taxed_price_sum + '</td>')
+            tableRow.append('<td>' + row.average_qty + '</td>')
+            tableRow.append('<td>' + row.average_price + '</td>')
+            tableRow.append('<td>' + row.average_discount + '</td>')
+            tableRow.append('<td>' + row.year + '</td>')
+            tableRow.append('<td>' + row.month + '</td>')
+            tableRow.append('<td>' + row.line_count + '</td>')
+            tableBody.append(tableRow)
+          }
         }
       })
     })
