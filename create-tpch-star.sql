5428aab1e5f5a02ea7a7e80166f4689877cfdd5d
j <redyarnspinner@gmail.com>
2018-04-14 17:25:31 +0800

    updated date creation table

--- create-tpch-star.sql
+++ create-tpch-star.sql
@@ -85,3 +85,10 @@ CREATE TABLE STAR_DATE AS
 SELECT datum, EXTRACT(month from datum) as month, EXTRACT(year from datum) as year
 FROM RAW_DATES;
 
+CREATE TABLE RAW_DATES as select datum::date from generate_series(timestamp without time zone '1950-01-01'::date, 
+									timestamp without time zone '2050-12-31'::date, '1 day') as gs(datum);
+									
+CREATE TABLE STAR_DATE AS
+SELECT datum, cast(EXTRACT(month from datum)  as int) as month, cast(EXTRACT(year from datum) as int) as year
+FROM RAW_DATES;
+
